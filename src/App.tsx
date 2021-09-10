9fc02895498ad7cd4debca9ed8b0b11205c1646b
Edualicerces10 <eduardoalicerces96@hotmail.com>
2021-09-10 14:40:38 +0100

    add render the header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+
+import { Header } from "./component/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <>
-      <div>Olá mundo!</div>
+      <Header />
       <GlobalStyle />
     </>
   )
