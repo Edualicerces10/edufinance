ab62625534cff889064c7a3a9d4247bbcd30d7d7
Edualicerces10 <eduardoalicerces96@hotmail.com>
2021-09-10 14:41:59 +0100

    creating the  header component

---
+++ src/component/Header/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background: var(--blue);
+`
+
+export const Content = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+`
